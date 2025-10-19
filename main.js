86db4f7217e92dbdcc306e617d6bfb98007b26dd
wltianyuxue <95035345+wltianyuxue@users.noreply.github.com>
2025-10-19 21:06:59 +0800

    main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const skins=['wukong-red.gif','wukong-blue.gif','wukong-green.gif'];
 let idx=0;
 const img=document.getElementById('s');
+const bgm = document.getElementById('bgm');
 /* 点击切换 */
 img.addEventListener('click',()=>{idx=(idx+1)%3;img.src=skins[idx];});
 /* 摇一摇切换 */
@@ -12,3 +13,8 @@ if(window.DeviceMotionEvent){
     if(delta>25){idx=(idx+1)%3;img.src=skins[idx];last=a.x+a.y+a.z;}
   });
 }
+/* 第一次点击：解除静音+播放 */
+  if (bgm.muted) {
+    bgm.muted = false;
+    bgm.play().catch(e => console.log(' autoplay blocked:', e));
+  }
